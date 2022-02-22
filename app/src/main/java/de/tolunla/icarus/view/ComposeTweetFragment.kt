b35f3dd813303189e76c1114e8d48a20efbbae64
Teslim Olunlade <tolunlade@outlook.com>
2022-02-22 09:17:45 -0500

    Added posting

--- app/src/main/java/de/tolunla/icarus/view/ComposeTweetFragment.kt
+++ app/src/main/java/de/tolunla/icarus/view/ComposeTweetFragment.kt
@@ -5,13 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.updateLayoutParams
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 import de.tolunla.icarus.databinding.DialogComposeTweetBinding
+import de.tolunla.icarus.db.dao.TweetDao
+import de.tolunla.icarus.db.entity.Tweet
+import de.tolunla.icarus.net.Twitter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ComposeTweetFragment : BottomSheetDialogFragment() {
+
+    private val jsonFormat = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+
     lateinit var binding: DialogComposeTweetBinding
+    var status = ""
+
+    @Inject
+    lateinit var twitter: Twitter
+
+    @Inject
+    lateinit var tweetDao: TweetDao
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +44,26 @@ class ComposeTweetFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DialogComposeTweetBinding.inflate(inflater)
+
+        binding.composeEdittext.addTextChangedListener { text ->
+            status = text.toString()
+            binding.sendTweet.isEnabled = status.isNotBlank()
+        }
+
+        binding.closeCompose.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        binding.sendTweet.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                twitter.postTweet(status)?.let { res ->
+                    val tweet = jsonFormat.decodeFromString<Tweet>(res)
+                    tweetDao.insertAll(listOf(tweet))
+                    dialog?.dismiss()
+                }
+            }
+        }
+
         return binding.root
     }
 
